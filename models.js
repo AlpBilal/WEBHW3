06409afd404250b5aa011c8dd83d102cc9ef53ce
Alpbilal <alpbilal67@gmail.com>
2025-12-16 16:15:23 +0300

    Made constructor

---
+++ models.js
@@ -0,0 +1,14 @@
+export class Student {
+    constructor(id, name, courses) {
+        Object.defineProperty(this, 'id', {
+            value: id,
+            writable: false,     
+            configurable: false,  
+            enumerable: true      
+        });
+
+        this.name = name;
+        this.courses = courses; 
+    }
+
+}
